97e21c20dfb3515b65ead0c4e41c22adb88c2c72
hai65 <vyastaroth123@gmail.com>
2025-07-14 15:05:18 +0700

    Preparing for the cluster: v6

--- app.py
+++ app.py
@@ -3,7 +3,13 @@ from layout import create_layout
 from router import register_routes
 from callbacks import register_callbacks
 
-app = dash.Dash(__name__, suppress_callback_exceptions=True)
+app = dash.Dash(
+    __name__,
+    suppress_callback_exceptions=True,
+    requests_pathname_prefix="/linkvana/",    # ✅ prefix khớp với Rancher hoặc Ingress
+    routes_pathname_prefix="/linkvana/"
+)
+
 app.title = "Linkvana"
 app.layout = create_layout()
 
@@ -12,5 +18,3 @@ register_callbacks(app)
 
 if __name__ == '__main__':
     app.run(debug=True, port=8051, host="0.0.0.0")
-    
-    
